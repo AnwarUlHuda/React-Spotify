9c74de944af2a7d9446b245a7b2753d42a848d13
Anwar ul Huda <50742069+AnwarUlHuda@users.noreply.github.com>
2022-11-10 01:20:37 +0530

    React spootify using Spotify Web API

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID="07c259efd2644f8da47e07d29f30a673",
+    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET="5629d790c23949d9bd0a1b61cd2e7452"
+  }
+}
