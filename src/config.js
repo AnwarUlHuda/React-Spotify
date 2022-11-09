087113633b62f083cbcaa71a8d0c9a9b31e563de
Anwar ul Huda <50742069+AnwarUlHuda@users.noreply.github.com>
2022-11-10 01:18:47 +0530

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'Your Spotify Client ID',
-    clientSecret: 'Your Spotify Client Secret'
-  }
-}
